9635764e51830a3e8992bf37dc1b37be7d9e8c85
Adrien Surowiec <adrien.surowiec@protonmail.com>
2022-12-21 17:19:02 +0100

    Create a look that removes books from the constructors array

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Array to store the constructors : 
 const constructorsArray = [];
+let numberOfBooks = -1;
 
 //Function that adds constructors to the above array : 
 function addConstructorToArray(constructor) {
@@ -12,13 +13,14 @@ function Book(title, name, year, rating, bookNr) {
   this.name = name;
   this.year = year;
   this.rating = rating;
+  this.bookNr = bookNr;
 }
 
 //Function to add books to the DOM : 
 function displayBook(array) {
   mainContainer.innerHTML += 
   `<div id=book-container} >
-      <img src="img/close.png" id="remove-book">
+      <img src="img/close.png" id="remove-book${array.bookNr}">
       <p>Title : ${array.title}</p>
       <p>Name : ${array.name}</p>
       <p>Year : ${array.year}</p>
@@ -26,15 +28,45 @@ function displayBook(array) {
   </div>`
 };
 
+
 const mainContainer = document.getElementById("main-container");
 const addForm = document.getElementById("add-form");
 const addButton = document.getElementById("add-button");
 const closeForm = document.getElementById("close-form");
 const addToMyBooksButton = document.getElementById("addToMyBooks");
 
+
+/* Attempt to construct the delete functionality through a function. Will now try to write a loop :  
+
+  function removeIt() {
+  const removeBook = document.getElementById('remove-book');
+  if (removeBook) {removeBook.addEventListener("click", function removeFromArray() {
+  console.log(constructorsArray);  
+  mainContainer.innerHTML = "";
+  console.log(constructorsArray);  
+  constructorsArray.pop();
+  console.log(constructorsArray);
+  constructorsArray.forEach(displayBook);
+  console.log("Removing Works");
+})
+}
+}; */
+
+function addRemoverLoop() {
+  for (let i=0; i<constructorsArray.length; i++) {
+  let removeBtn = document.getElementById(`remove-book${i}`);
+  console.log(removeBtn);
+  console.log(constructorsArray);
+  removeBtn.addEventListener("click", function() {
+    constructorsArray.splice(i, 1);
+    console.log("The result after the splice is : ");
+    console.log(constructorsArray);
+  });
+}
+}
+
 addButton.addEventListener("click", function showForm() {
   addForm.style.display = 'grid';
-  console.log("Add wokrs")
 });
 
 closeForm.addEventListener("click", function hideForm() {
@@ -42,9 +74,12 @@ closeForm.addEventListener("click", function hideForm() {
 });
 
 addToMyBooksButton.addEventListener("click", function grabBookInfo () {
-  addConstructorToArray(new Book(document.getElementById("title").value, document.getElementById("name").value, document.getElementById("year").value, document.getElementById("rating").value));
+  numberOfBooks += 1;
+  addConstructorToArray(new Book(document.getElementById("title").value, document.getElementById("name").value, document.getElementById("year").value, document.getElementById("rating").value, numberOfBooks));
   addForm.style.display = 'none';
   mainContainer.innerHTML = "";
   constructorsArray.forEach(displayBook);
+  console.log(constructorsArray);
   addForm.reset();
+  addRemoverLoop();
 });
